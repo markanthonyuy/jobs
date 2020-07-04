bb1cee4a54300a7c562d4bebab9bcb1cac4e1473
markanthonyuy <macmac.uy@gmail.com>
2020-07-04 14:49:42 +0800

    update proxy

--- functions/proxy.js
+++ functions/proxy.js
@@ -3,12 +3,12 @@ exports.handler = async function (event, context, callback) {
     const res = await fetch('https://jobs.github.com/positions.json')
     const data = await res.json()
     console.log(data)
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    })
   } catch (e) {
     console.log(e)
   }
-
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World',
-  })
 }
