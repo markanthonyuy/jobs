a0080d5d5c0cbc0fadf2de42ab460e03cc9b0eb7
markanthonyuy <macmac.uy@gmail.com>
2020-07-04 15:30:57 +0800

    add proxt URL params

--- functions/proxy.js
+++ functions/proxy.js
@@ -2,7 +2,15 @@ const fetch = require('node-fetch')
 
 exports.handler = async function (event, context, callback) {
   try {
-    const res = await fetch('https://jobs.github.com/positions.json')
+    const description = event.queryStringParameters.description
+      ? `description=${event.queryStringParameters.description}`
+      : ''
+    const location = event.queryStringParameters.location
+      ? `location=${event.queryStringParameters.location}`
+      : ''
+    const res = await fetch(
+      `https://jobs.github.com/positions.json?${description}${location}`
+    )
     const data = await res.json()
     console.log(data)
 
